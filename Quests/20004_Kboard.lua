d7d6847939e6375c5a9644b76373b12b0fb775fb
Emre Kira Sari <efsaneemoo1998@gmail.com>
2019-08-06 12:10:54 +0300

    06.08.2019 Update New 2
    06.08.2019 Update New  2

--- Quests/20004_Kboard.lua
+++ Quests/20004_Kboard.lua
@@ -2,7 +2,7 @@ local Ret = 0;
 local NPC = 20004;
 
 if (EVENT == 100) then
-	SelectMsg(UID, 3, -1, 1377, NPC, 4609, 102, 4262, 104);
+	SelectMsg(UID, 3, -1, 1377, NPC, 4609, 102);
 end
 
 if (EVENT == 102) then
@@ -11,8 +11,4 @@ end
 
 if (EVENT == 102) then
 	UserEvent(UID,1)
-end
-
-if (EVENT == 104) then
-	PlayerEffect(UID,30403)
 end
